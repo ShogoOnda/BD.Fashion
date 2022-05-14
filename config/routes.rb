268f548d72b215f7118a7c2ac421e70eb697b795
ShogoOnda <duo0116@gmail.com>
2022-05-14 07:00:18 +0000

    [Add] devise User Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+# 顧客用
+# URL /customers/sign_in ...
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
