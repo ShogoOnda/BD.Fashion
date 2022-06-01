80aa1d906a127cf99532038918cfb413ed29af8f
ShogoOnda <duo0116@gmail.com>
2022-06-01 14:51:03 +0000

    [Add] Item各種画面、コントローラー

--- app/controllers/public/outers_controller.rb
+++ app/controllers/public/outers_controller.rb
@@ -1,2 +1,44 @@
 class Public::OutersController < ApplicationController
+
+  def new
+    @outer = Outer.new
+  end
+
+  def create
+    @outer = Outer.new(outer_params)
+    if @outer.save
+      redirect_to outer_path(@outer.id)
+    else
+      @outers = Outer.all
+      render :index
+    end
+  end
+
+  def index
+    @outers = Outer.all
+    @outer = Outer.new
+  end
+
+  def show
+    @outer = Outer.find(params[:id])
+  end
+
+  def edit
+    @outer = Outer.find(params[:id])
+  end
+
+  def update
+    @outer = Outer.find(params[:id])
+    if @outer.update(outer_params)
+      redirect_to outer_path(@outer.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def outer_params
+    params.require(:outer).permit(:outerImage, :name, :size, :color, :shoulder, :width, :length, :sleeve, :maxtemperature, :mintemperature)
+  end
+
 end
