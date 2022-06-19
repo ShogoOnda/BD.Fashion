588e7e4867eeda0ea92569285173570f1212b8c1
ShogoOnda <duo0116@gmail.com>
2022-06-19 12:40:14 +0000

    [Finish] アプリケーション

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-  email: "duo0116@gmail.com",
+  email: "admin@gmail.com",
   password: "admin0116"
 )
 
