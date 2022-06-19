db552ea3bcfb76e4c717ddc37ca00b6f36e5ae63
ShogoOnda <duo0116@gmail.com>
2022-06-19 11:02:48 +0000

    [Fix] homesコントローラーリファクタリング

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,42 +1,5 @@
 class Public::HomesController < ApplicationController
 
-  def index
-    @onedays = Oneday.all
-    @oneday = Oneday.new
-  end
-
-  def new
-    @oneday = Oneday.new
-  end
-
-  def show
-    @oneday= Oneday.find(params[:id])
-  end
-
-  def create
-    Oneday.create(oneday_parameter)
-    redirect_to onedays_path
-  end
-
-  def destroy
-    @oneday = Oneday.find(params[:id])
-    @oneday.destroy
-    redirect_to onedays_path, notice:"削除しました"
-  end
-
-  def edit
-    @oneday = Oneday.find(params[:id])
-  end
-
-  def update
-    @oneday = Oneday.find(params[:id])
-    if @oneday.update(oneday_parameter)
-      redirect_to onedays_path, notice: "編集しました"
-    else
-      render 'edit'
-    end
-  end
-
 # 以下を追加
   def guest_sign_in
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
@@ -48,10 +11,5 @@ class Public::HomesController < ApplicationController
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 
-  private
-
-  def oneday_parameter
-    params.require(:oneday).permit(:title, :outer_id, :inner_id, :bottom_id, :shoe_id, :start_time)
-  end
 end
 
