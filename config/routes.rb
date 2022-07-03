36671f4103cffa3320ff8bd45faeb48a277fd362
ShogoOnda <duo0116@gmail.com>
2022-07-03 12:26:43 +0000

    [Fix] routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     root :to =>"homes#top"
     get "/about"=>"homes#about", as: 'about'
     post '/homes/guest_sign_in', to: 'homes#guest_sign_in'
-    get "/users/my_page" => "users#show", as: "my_page"
+      get "/users/my_page" => "users#show", as: "my_page"
     get "/users/unsubscribe"=>"users#unsubscribe", as: 'unsubscribe'
     patch "/users/withdraw" => "users#withdraw", as: 'withdraw'
     resources :users
